aace7b7e5107cd44ffd5eb7191eac1c71784f648
Mahdi Ghiasi <mmghf74@gmail.com>
2023-06-03 12:18:10 +0330

    hyperparameter setting done by cross validation testing

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,5 @@
 import math
 import numpy as np
-import pandas as pd
 
 outlier_threshold = 0.0001
 
--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import helper_functions as hf
-import pandas as pd
-
-
-if __name__ == "__main__":
-    df = pd.read_csv("Dataset2.csv")
-    print(hf.ExtractValues(df["poisonous"]))
